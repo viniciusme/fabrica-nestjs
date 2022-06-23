58928931661946dc69b6db81479b81103b13ba25
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-22 22:37:10 -0300

    Implementing improvements to the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+const { version, name } = require('../package.json');
+const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+
+  await app.listen(port);
+
+  logger.log(
+    `<<< ${name} v${version} foi iniciado no ambiente "DEV" >>> ${await app.getUrl()}`,
+  );
 }
 bootstrap();
